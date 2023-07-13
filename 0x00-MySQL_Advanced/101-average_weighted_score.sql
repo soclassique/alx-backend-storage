4c9b4497883656de8b5949b1dff6a65e91c9f5f3
Soso <soclassique@yahoo.com>
2023-07-13 04:23:18 +0000

    sql

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,12 @@
+-- Average weighted score
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers() BEGIN
+UPDATE users
+SET average_score = (
+    SELECT sum(p.weight * c.score) / sum(p.weight)
+    FROM projects AS p
+      INNER JOIN corrections AS c ON p.id = c.project_id
+    WHERE c.user_id = users.id
+  );
+END;
+DELIMITER; //
